726273ca3542de51f68d259be65ead0f814a2d09
Mikhail <dadykinm@yandex.ru>
2021-02-25 19:54:52 +0200

    31

---
+++ script/allure.sh
@@ -0,0 +1,2 @@
+/Volumes/WDCWD/Downloads/selenium/allure/bin/allure serve allure-report/
+
---
+++ script/tests.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+cd ..
+
+# Собираем image с тегом my_tests
+docker build -t my_test2 .
+
+# Запускаем контейнер под именем my_run из image my_tests
+docker run --name my_test21 my_test2 --browser chrome
+
+# Копируем из контейнера созданный allure-result
+docker cp my_test21:/app/allure-result .
+
+# Запускаем хост для отчёта аллюр (утилита лежит локально)
+# Хост отчёта нужно будет остановить руками
+# Сылка на алюр должна быть своя
+/Volumes/WDCWD/Downloads/selenium/allure/bin/allure serve allure-result/
+
+# Удаляем из системы созданный контейнер
+#docker system prune -f
